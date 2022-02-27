a28306d42ef3ed9b388e8c3723bb455c7db210e0
faiz8411 <faizullah.noa2014@gmail.com>
2022-02-27 21:50:50 +0600

    side bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Dashboard from './components/Dashboard/Dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <h2>dashboard</h2>
+     <Dashboard></Dashboard>
     </div>
   );
 }
